5aeea02a234a8baaf061c1ed6e060dde9d183ddc
Sahej Singh <sahejggmu@gmail.com>
2018-12-12 17:11:14 +0530

    Update pal.py

--- pal.py
+++ pal.py
@@ -1,9 +1,3 @@
-t = int(input("Enter value"))
-test_inputs = []
-for i in range(1,t+1):
-	a = int(input("Enter value"))
-	test_inputs.append(a)
-print(test_inputs)
 def pal_find(c):
 	x = c + 1
 	while x > c:
@@ -12,7 +6,7 @@ def pal_find(c):
 			return(x)
 			break
 		x += 1
-for x in test_inputs:
-	print(pal_find(int(x)))
+for i in range(int(input("Enter value: "))):
+	print(pal_find(int(input("value"))))
 
 			
